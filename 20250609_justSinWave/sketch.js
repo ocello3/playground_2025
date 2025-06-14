9aa50e69beb9bff9ecbfeb681d0b3ce2acdffb37
ocello3 <ocello3@users.noreply.github.com>
2025-06-14 14:10:47 +0900

    almost done with vertex and coloring
    back to home from Utsunomiya, day 2

--- 20250609_justSinWave/sketch.js
+++ 20250609_justSinWave/sketch.js
@@ -3,7 +3,8 @@ import "../lib/p5.sound.min.js";
 import * as u from "./util.js";
 
 /*
-
+音が出るか確認
+末尾配列のx座標を使ってない
 */
 
 const sketch = (s) => {
@@ -13,14 +14,19 @@ const sketch = (s) => {
 		isInit: true,
 		vol: 0,
 		frameRate: 0,
+		fingers: 3,
+		colors: [s.color(123, 108, 103), s.color(213, 85, 33), s.color(138, 37, 27)],
 	};
 	s.setup = () => {
 		u.initRoutine(s);
 		size = u.getSize(s);
 		snd = (()=> {
 			const snd = {};
-			snd.osc = new p5.Oscillator('sine');
-			snd.osc.amp(0);
+			snd.oscs = [...Array(p.fingers)].map(() => {
+				const osc = new p5.Oscillator('sine');
+				osc.amp(0);
+				return osc;
+			});
 			return snd;
 		})();
 		// s.frameRate(10);
@@ -33,20 +39,20 @@ const sketch = (s) => {
 	s.draw = () => {
 		function getDt(_dt) {
 			dt = {};
-			dt.isTouch = s.touches.length != 0;
 			const _tracks = p.isInit ?
-				[...Array(3)].map(() => Array(50).fill(0)) :
+				[...Array(p.fingers)].map(() => Array(50).fill(0)) :
 				_dt.tracks;
 			dt.tracks = _tracks.map((_track, index) =>
-				s.touches.length > index ?
+				(s.touches.length > index && s.frameCount % 3 === 0) ?
 					[s.touches[index], ..._track.slice(0, -1)] :
 					[0, ..._track.slice(0, -1)]);
-			dt.snd = (() => {
+			dt.snds = dt.tracks.map((track) => {
 				const snd = {};
-				snd.pan = dt.isTouch ? s.map(s.mouseX, 0, size, -1, 1) :  size * 0.5;
-				snd.vol = dt.isTouch ? s.map(s.mouseY, 0, size, 0, 1) : 0;
+				snd.pan = (track[0] === 0) ? 0 : s.map(track[0].x, 0, size, -1, 1);
+				snd.vol = (track[0] === 0) ? 0 : s.map(track[0].y, 0, size, 0, 0.8);
+				snd.freq = (track.at(-1) === 0) ? 0 : s.map(track.at(-1).y, 0, size, 50, 1000);
 				return snd;
-			})();
+			});
 			return dt;
 		}
 		dt = getDt(dt);
@@ -60,20 +66,37 @@ const sketch = (s) => {
 		}
 		routine();
 		function drawTracks() {
+			// track line
 			dt.tracks.forEach((track, trackIndex) => {
+				s.push();
 				s.beginShape();
-				s.stroke(50 * (trackIndex +1));
+				s.noStroke();
+				s.fill(p.colors[trackIndex]);
 				track.forEach((point) => {
 					if (point === 0) return ;
 					s.vertex(point.x, point.y);
 				});
 				s.endShape();
-				});
+				s.pop();
+			});
+			// tie head and tail
+			s.push();
+			s.beginShape();
+			s.fill(240, 220);
+			dt.tracks.forEach((track) => {
+				if (track[0] != 0) s.vertex(track.at(-1).x, track[0].y);
+				if (track.at(-1) != 0) s.vertex(track.at(-1).x, track.at(-1).y);
+			});
+			s.endShape(s.CLOSE);
+			s.pop();
 		}
 		drawTracks();
 		function playSnd() {
-			snd.osc.pan(dt.snd.pan);
-			snd.osc.amp(dt.snd.vol);
+			snd.oscs.forEach((osc,index) => {
+				osc.pan(dt.snds[index].pan);
+				osc.amp(dt.snds[index].vol);
+				osc.freq(dt.snds[index].freq);
+			})
 		}
 		playSnd();
 	};
